c57881184daa85d6934ee4d967d44c50862adc67
Grey Nagle <greynagle@gmail.com>
2020-07-07 20:45:13 -0400

    setting up for vercel

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000',
+  API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000',
   API_KEY: process.env.REACT_APP_API_KEY
 
 }
+// https://agile-ocean-59400.herokuapp.com/
