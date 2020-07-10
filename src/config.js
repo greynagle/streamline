f326bdd338c51b889350207d33fd6b3e1994d1d8
Grey Nagle <greynagle@gmail.com>
2020-07-09 23:20:00 -0400

    minor

--- src/config.js
+++ src/config.js
@@ -1,7 +1,4 @@
 export default {
-  API_ENDPOINT: "https://agile-ocean-59400.herokuapp.com",
+  API_ENDPOINT: "https://serene-journey-40457.herokuapp.com",
   API_KEY: process.env.REACT_APP_API_KEY
-
 }
-// https://agile-ocean-59400.herokuapp.com/
-// process.env.API_PORT || 'https://agile-ocean-59400.herokuapp.com',
